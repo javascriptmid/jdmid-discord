f99717e6747a98c196dafd19f30cc11f35b6c5f5
Jerome Olvera <jerome.olvera@gmail.com>
2021-03-13 16:06:49 -0600

    Refactor bot interface

--- bot.ts
+++ bot.ts
@@ -1,6 +1,8 @@
 import * as Discord from "discord.js";
+import { some, none, PromiseResult } from "./result";
 import { DISCORD_BOT_TOKEN } from "./config";
 import createVoiceChannel from "./create-voice-channel";
+import deleteVoiceChannel from "./delete-voice-channel";
 
 const client = new Discord.Client();
 
@@ -9,18 +11,45 @@ client.once("ready", () => {
   console.log("Client logged in... Setting up client.");
 });
 
+enum Commands {
+  CreateVoiceChannel = "create-voice-channel",
+  DeleteVoiceChannel = "delete-voice-channel",
+}
+
 client.on("message", async (context) => {
-  const [command, category, title] = context.content.split(" ");
-  if (command === "!create-channel") {
-    const result = await createVoiceChannel(context, {
-      category,
-      title,
-    });
-
-    if (result.kind === "failure") {
-      return context.channel.send(result.error);
-    }
-
-    return context.channel.send(result.value);
+  const [identity, command, ...args] = context.content.split(" ");
+  if (identity !== "meetbot") return;
+  const result = await executeCommand(context, command as Commands, args);
+  if (result.kind === "failure") {
+    return context.channel.send(result.error);
   }
+
+  return context.channel.send(result.value);
 });
+
+async function executeCommand(
+  context: Discord.Message,
+  command: Commands,
+  args: string[]
+): PromiseResult<string, string> {
+  switch (command) {
+    case Commands.CreateVoiceChannel: {
+      const [category, title] = args;
+      return createVoiceChannel(context, {
+        category,
+        title,
+      });
+    }
+    case Commands.DeleteVoiceChannel: {
+      const [category, title] = args;
+      return deleteVoiceChannel(context, {
+        category,
+        title,
+      });
+    }
+    default: {
+      if (context.author.bot) return none();
+      return some(":C");
+    }
+  }
+}
