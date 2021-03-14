4c86957465f4b3737ef71f5aeb2fb29c3e5df98e
Jerome Olvera <jerome.olvera@gmail.com>
2021-03-13 18:10:57 -0600

    Add yargs for arg parsing

--- bot.ts
+++ bot.ts
@@ -1,10 +1,13 @@
 import * as Discord from "discord.js";
+import * as yargs from "yargs";
 import { some, none, PromiseResult } from "./result";
 import { DISCORD_BOT_TOKEN } from "./config";
 import createVoiceChannel from "./create-voice-channel";
 import deleteVoiceChannel from "./delete-voice-channel";
+import cleanUpChannel from "./clean-up-channel";
 
 const client = new Discord.Client();
+const app = yargs.command("meetbot <command> [category] [title]", "");
 
 client.login(DISCORD_BOT_TOKEN);
 client.once("ready", () => {
@@ -14,12 +17,16 @@ client.once("ready", () => {
 enum Commands {
   CreateVoiceChannel = "create-voice-channel",
   DeleteVoiceChannel = "delete-voice-channel",
+  CleanUpChannel = "clean-up-channel",
 }
 
 client.on("message", async (context) => {
-  const [identity, command, ...args] = context.content.split(" ");
-  if (identity !== "meetbot") return;
-  const result = await executeCommand(context, command as Commands, args);
+  const { _: root, command, ...options } = app.parse(context.content);
+  if (root[0] !== "meetbot") {
+    return;
+  }
+
+  const result = await executeCommand(context, command as Commands, options);
   if (result.kind === "failure") {
     return context.channel.send(result.error);
   }
@@ -30,19 +37,26 @@ client.on("message", async (context) => {
 async function executeCommand(
   context: Discord.Message,
   command: Commands,
-  args: string[]
+  options: any
 ): PromiseResult<string, string> {
   switch (command) {
     case Commands.CreateVoiceChannel: {
-      const [category, title] = args;
+      const { category, title } = options;
       return createVoiceChannel(context, {
         category,
         title,
       });
     }
     case Commands.DeleteVoiceChannel: {
-      const [category, title] = args;
+      const { category, title } = options;
       return deleteVoiceChannel(context, {
+        category,
+        title,
+      });
+    }
+    case Commands.CleanUpChannel: {
+      const { category, title } = options;
+      return cleanUpChannel(context, {
         category,
         title,
       });
